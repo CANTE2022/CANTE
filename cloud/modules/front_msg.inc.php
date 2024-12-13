e16fffc5bbf676111336a13ea51c65f0b3eff39c
CANTE <100255692+CANTE2022@users.noreply.github.com>
2024-12-13 18:33:30 +0800

    Add files via upload

---
+++ cloud/modules/front_msg.inc.php
@@ -0,0 +1,9 @@
+<?php
+#	$Id: front_msg.inc.php 26 2012-03-29 10:33:52Z along $
+#
+if(!defined('IN_PHPDISK')) {
+	exit('[PHPDisk] Access Denied');
+}
+
+require_once template_echo('front_msg',$user_tpl_dir);
+?>
---
+++ cloud/plugins/api/install.inc.php
@@ -0,0 +1,22 @@
+<?php 
+##
+#	Project: PHPDISK File Storage Solution
+#	This is NOT a freeware, use is subject to license terms.
+#
+#	Site: http://www.phpdisk.com
+#
+#	$Id: install.inc.php 9 2012-02-04 02:17:20Z along $
+#
+#	Copyright (C) 2008-2012 PHPDisk Team. All Rights Reserved.
+#
+##
+if(!defined('IN_PHPDISK')) {
+	exit('[PHPDisk] Access Denied');
+}
+function install_plugin(){
+	return true;
+}
+function uninstall_plugin(){
+	return true;
+}
+?>
