57da57afb2f341ddb75b5e739bd1d85ff515d0fb
CANTE <100255692+CANTE2022@users.noreply.github.com>
2024-12-14 11:25:21 +0800

    Delete cloud directory

--- cloud/modules/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if( $_SERVER['HTTP_REFERER'] == "" ){
-header("");
-echo '<script type="text/javascript">window.location.href="/"</script>'; 
-exit('´íÎó');}
-
-?>
--- cloud/zhmm.php
+++
@@ -1,6 +0,0 @@
-<?php 
-//重定向浏览器 
-header("Location: https://jq.qq.com/?_wv=1027&k=5wMmFPc"); 
-//确保重定向后，后续代码不会被执行 
-exit;
-?>
